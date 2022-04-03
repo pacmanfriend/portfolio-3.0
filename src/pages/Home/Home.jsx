0ee7e529c7f0b39807aa91bed9117b05149714ee
Владислав Антонов <lolik1806@gmail.com>
2022-04-03 20:09:49 +0300

    home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,48 +1,25 @@
 import React from 'react';
 
 import styles from './Home.module.css';
+import logo_2 from '@images/logo/logo_2.jpg';
+import classNames from 'classnames';
 
-import {Photo} from "@components";
-import photo from '@images/photo.jpg';
-
+let cx = classNames.bind(styles);
 
 const Home = () => {
+    let infoTitleClass = classNames(styles.infoItem, styles.title);
+    let infoTextClass = classNames(styles.infoItem, styles.text);
+    let infoCopyrightClass = classNames(styles.infoItem, {[styles.copyright]: true})
+
     return (
         <div className={styles.home}>
-            <h1>Главная</h1>
-            <div className={styles.homeContainer}>
-                <div className={styles.photoContainer}>
-                    <Photo src={photo} alt={"Фото"}/>
-                </div>
-                <div className={styles.infoContainer}>
-                    <div>
-                        <span>Модель: </span>
-                        <span>Т-800</span>
-                    </div>
-                    <div>
-                        <span>Прозвище: </span>
-                        <span>"Дядя Боб"</span>
-                    </div>
-                    <div>
-                        <span>Вес: </span>
-                        <span>180кг</span>
-                    </div>
-                    <div>
-                        <span>Вид: </span>
-                        <span>Андроид</span>
-                    </div>
-                    <div>
-                        <span>Пол: </span>
-                        <span>Мужской</span>
-                    </div>
-                    <div>
-                        <span>Род занятий: </span>
-                        <span>Убийца, инфильтратор</span>
-                    </div>
-                    <div>
-                        <a href='https://ru.wikipedia.org/wiki/T-800' target={"_blank"}>Больше информации...</a>
-                    </div>
-                </div>
+            <div className={styles.info}>
+                <span className={infoTitleClass}>Cyberdyne Systems</span>
+                <span className={infoTextClass}>Разработка и производство киборгов.</span>
+                <span className={infoCopyrightClass}>©Cyberdyne Systems, 1984-2022.</span>
+            </div>
+            <div className={styles.logoContainer}>
+                <img className={styles.logo} src={logo_2} alt={'Cyberdyne Systems'}/>
             </div>
         </div>
     );
