bf7d5a806dd8fa04c531b038e03470f6ee6c91f3
Владислав Антонов <lolik1806@gmail.com>
2022-04-04 17:26:53 +0300

    init cart and catalog

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,2 +1,4 @@
 export {default as Home} from './Home/Home';
 export {default as TerminatorCard} from './TerminatorCard/TerminatorCard';
+export {default as Catalog} from './Catalog/Catalog';
+export {default as Cart} from './Cart/Cart';
