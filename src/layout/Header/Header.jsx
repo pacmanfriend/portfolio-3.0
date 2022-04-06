68c8e05385827de3607cddae2cc31928a36fc0c6
Владислав Антонов <lolik1806@gmail.com>
2022-04-06 06:49:08 +0300

    add router links in Header.jsx

--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import {NavLink} from "react-router-dom";
+import {Outlet} from "react-router";
 
 import styles from './Header.module.css';
 
 const Header = () => {
     return (
-        <div className={styles.header}>
-            <NavLink className={styles.item} to={'/'}>Главная</NavLink>
-            <NavLink className={styles.item} to={'/#'}>Каталог</NavLink>
-            <NavLink className={styles.item} to={'/##'}>Корзина</NavLink>
-        </div>
+        <>
+            <div>
+                <nav className={styles.header}>
+                    <NavLink className={styles.item} to={'/'}>Главная</NavLink>
+                    <NavLink className={styles.item} to={'/catalog'}>Каталог</NavLink>
+                    <NavLink className={styles.item} to={'/cart'}>Корзина</NavLink>
+                </nav>
+            </div>
+            <div>
+                <Outlet/>
+            </div>
+        </>
     );
 };
 
