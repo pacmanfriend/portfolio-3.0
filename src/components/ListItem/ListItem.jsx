8f4d1db5148147dacd59963893660b6626b2e213
Владислав Антонов <lolik1806@gmail.com>
2022-04-06 16:49:17 +0300

    add catalog reducer

--- src/components/ListItem/ListItem.jsx
+++ src/components/ListItem/ListItem.jsx
@@ -4,11 +4,15 @@ import styles from './ListItem.module.css';
 import {Photo} from "@components";
 import {useLocation, useNavigate} from "react-router";
 import {CommonButton} from "@UI";
+import {useDispatch} from "react-redux";
 
 const ListItem = ({terminator, children, ...props}) => {
     let navigate = useNavigate();
     let location = useLocation();
     let from = location.state?.from?.pathname || `/card`;
+
+    let dispatch = useDispatch();
+    let cart = useLocation();
 
     let toCard = () => {
         navigate(from, {
@@ -19,16 +23,19 @@ const ListItem = ({terminator, children, ...props}) => {
     }
 
     let addToCart = () => {
-
+        dispatch({type: "ADD_ITEM", payload: terminator})
     }
 
+
     return (
-        <div onClick={toCard} className={styles.card}>
-            <div>
-                <Photo src={terminator.photoUrl} alt={terminator.name}/>
-            </div>
-            <div className={styles.nameContainer}>
-                <span>{terminator.name}</span>
+        <div className={styles.card}>
+            <div onClick={toCard}>
+                <div>
+                    <Photo src={terminator.photoUrl} alt={terminator.name}/>
+                </div>
+                <div className={styles.nameContainer}>
+                    <span>{terminator.name}</span>
+                </div>
             </div>
             <div className={styles.buttonContainer}>
                 <CommonButton onClick={addToCart} text={'Добавить в корзину'}/>
