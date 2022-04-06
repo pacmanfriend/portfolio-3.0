56b565b9afdc9b04f3a43528cf2da8f195630699
Владислав Антонов <lolik1806@gmail.com>
2022-04-06 16:47:21 +0300

    cart

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import styles from './Cart.module.css';
+import {useDispatch, useSelector} from "react-redux";
+import CartItem from "@components/CartItem/CartItem";
 
 const Cart = () => {
+    let dispatch = useDispatch();
+    let cartItems = useSelector(state => state.cart.cartItems)
+
+
     return (
-        <div>
-            Корзина
+        <div className={styles.cart}>
+            <div className={styles.title}>
+                <h1>
+                    Корзина
+                </h1>
+            </div>
+            <div className={cartItems}>
+                {cartItems.map((item, i) => {
+                    return <CartItem terminator={item} key={i * 0.25}/>
+                })}
+            </div>
         </div>
     );
 };
